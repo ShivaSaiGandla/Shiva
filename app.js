5cd533d0e64d3abb8e50c629942daa21a474c5b7
ShivaSaiGandla <shivasaig248@gmail.com>
2026-06-11 13:18:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,12 +26,12 @@ app.get('/', (req, res) => {
 // Route for POST requests
 app.post('/', (req, res) => {
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
-  console.log(\n\nWebhook received ${timestamp}\n);
+  console.log(`\nWebhook received ${timestamp}\n`);
   console.log(JSON.stringify(req.body, null, 2));
   res.status(200).end();
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(\nListening on port ${port}\n);
+  console.log(`\nListening on port ${port}\n`);
 });
